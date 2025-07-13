fa1f456924fe7cdfdf4708ff1e1a15eaacdfcd8c
Resham <reshamupadhyay48@gmail.com>
2025-07-13 23:24:59 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,21 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import SendMoney from "./pages/SendMoney";
 
 function App() {
-
   return (
-    <div></div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/send" element={<SendMoney />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;

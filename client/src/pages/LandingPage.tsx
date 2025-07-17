457f5b8e56634e9cbe6838ee284000f39ef43490
Resham <reshamupadhyay48@gmail.com>
2025-07-17 23:54:47 +0530

    Update LandingPage.tsx

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,3 +1,4 @@
+import FeatureSection from "../components/FeatureSection";
 import {HeroSection} from "../components/HeroSection";
 
 import { NavBar } from "../components/NavBar";
@@ -6,7 +7,8 @@ export default function LandingPage() {
   return (
     <div className="w-full h-auto bg-surface min-h-screen">
       <NavBar />
-      <HeroSection title="Your Wallet, Upgraded."/>        
+      <HeroSection title="Your Wallet, Upgraded."/>    
+      <FeatureSection/>    
     </div>
   );
 }
